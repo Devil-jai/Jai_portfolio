c1374dcbebd43d018e4b18402d9ee59f2e578f95
Jai Bagaria <deviljai1999@gmail.com>
2025-07-01 12:03:23 +0530

    12:03

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -7,7 +7,7 @@ const ScrollToTop = () => {
   useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth" // optional: for smooth scrolling
+      behavior: "smooth" 
     });
   }, [pathname]);
 
